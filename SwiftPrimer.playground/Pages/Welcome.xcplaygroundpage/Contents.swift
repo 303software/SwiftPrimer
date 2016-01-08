2f0344b4ce4db7b25636d5d2dd60fb29be7fc340
Chris Chares <chris.chares@303software.com>
2016-01-08 13:46:07 -0700

    real links

--- SwiftPrimer.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ SwiftPrimer.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -1,19 +1,30 @@
 /*: 
 # Welcome to Swift
 
-[Swift Reference]:http://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language
+[Swift Book]:http://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language
 
-The [Swift Reference] is going to be your friend.  Highly recommend reading it front to back.  Please play with all the examples in here, it's a great way to learn.  If you get stuck and can't get a page to compile again, just clear your local changes in git.  `git checkout -- *`
+The [Swift Book] is going to be your friend.  Highly recommend reading it front to back.  Please play with all the examples in here, it's a great way to learn.  
+If you get stuck and can't get a page to compile again, just clear your local changes in git.  `git checkout -- *`
 
-1. Basics
-2. Optionals
-3. Functions
-4. Classes & Structs
-5. Enums
-6. Protocols
-7. Tuples
-8. Generics
-9. Error Handling
-10. Dad?
+# **Resources**
+
+----
+
++ [Swift Book]
+
+
+# **Chapters**
+
+----
+
+1. [Basics](1.%20Basics)
+2. [Optionals](2.%20Optionals)
+3. [Functions](3.%20Functions)
+4. [Classes & Structs](4.%20Classes%20&%20Structs)
+5. [Enums](5.%20Enums)
+6. [Protocols](6.%20Protocols)
+7. [Tuples](7.%20Tuples)
+8. [Generics](8.%20Generics)
+9. [Error Handling](9.%20Error%20Handling)
 */
 //: [Next](@next)
